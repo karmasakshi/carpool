0907cc454c69ee18b1fc60c008ba34c5fe6cf8e0
Shivani <shivani.chelliah@publicissapient.com>
2019-05-09 15:45:42 +0400

    create results page for guest

--- src/Header.js
+++ src/Header.js
@@ -12,7 +12,7 @@ import Results from './Results';
 
 class Header extends Component{
   
-   state = {log: null, users:[], uid: localStorage.getItem("appTokenKey")};
+   state = {log: null, users:[], uid: localStorage.getItem("appTokenKey"), currentUser:[], otherUsers:[], results: []};
 
    componentDidMount() {
     fire.auth().onAuthStateChanged((user) => {     //to detect when a user is authenticated
@@ -45,8 +45,57 @@ class Header extends Component{
             });
           }
 
+          let usr = newState.filter((index) => {
+
+            return index.uid == user.uid;
+      
+          });
+
+          
+          if(usr.length>0)  //if profile is created by user, this will be executed, else no
+          {
+            let otherUsers = newState.filter((index) => {
+
+            return ((index.uid !== this.state.uid) && (index.role !== usr[0].role));
+            });
+ 
+            var lat=usr[0].lat;
+           var long=usr[0].lng;
+           var doubleLat=parseFloat(lat);
+           var doubleLong=parseFloat(long);
+
+           function deg2rad(deg) {
+            return deg * (Math.PI/180)
+          }
+
+          let results = [];
+            for(var i=0;i<otherUsers.length;i++){
+              var R = 6371; // km 
+              var x1 = parseFloat(otherUsers[i].lat)-doubleLat;
+              console.log("x1",x1);
+              var dLat = deg2rad(x1)  
+              var x2 = parseFloat(otherUsers[i].lng)-doubleLong;
+              var dLon = deg2rad(x2);
+              console.log("x2",x2);
+  
+              var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+                              Math.cos(deg2rad(doubleLat)) * Math.cos(deg2rad(otherUsers[i].lat)) * 
+                              Math.sin(dLon/2) * Math.sin(dLon/2);  
+              var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+              var d = R * c; 
+            console.log("a: ",a);
+            console.log("C:",c);
+            console.log("D", d);
+              
+            console.log(results.push({firstName: otherUsers[i].firstName, lastName: otherUsers[i].lastName, distance: d}));
+            console.log("Results ",results);
+            console.log("Testdd",this.state.results);
+          }
+            this.setState({otherUsers: otherUsers, results: results});
+          }
+
           this.setState({
-            users: newState, log: true
+            users: newState, log: true, currentUser: usr
           })
           });
      } 
@@ -65,7 +114,7 @@ class Header extends Component{
        <Switch>
        <Route exact path='/'component={Home}/>
        <Route exact path='/signin' render={() => <SignIn log={this.state.log}/>} />
-       <Route exact path='/results' render={() => <Results users={this.state.users} log={this.state.log} uid={this.state.uid}/>}  />
+       <Route exact path='/results' render={() => <Results results={this.state.results}/>}  />
        <Route exact path='/signup' component={CreateAccount}/>
        <Route exact path='/users' component={CreateProfile}/>
        <Route render={() => <h1>Page not found</h1>} />
--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import './index.css'
 import '../node_modules/semantic-ui-css/semantic.min.css';
-import { Grid, Image, Card, Icon } from 'semantic-ui-react'
+import { Grid, Image, Card, Icon, Button} from 'semantic-ui-react'
 
 class Results extends Component {
 
@@ -11,139 +11,36 @@ class Results extends Component {
 
     super(props);
 
-    this.state = { currentUser: undefined, users: [] };
+    this.state = { results: this.props.results };
 
   }
 
-  setUser() {
-
-    let usr = this.props.users.filter((index) => {
-
-      return index.uid === this.props.uid;
-
-    });
-
-    this.setState({ currentUser: usr });
-    console.log(this.state.currentUser);
-
+  componentWillReceiveProps(nextProps){
+    
+    console.log(2);
+    if(this.props != nextProps) {
+      this.setState({
+        results: nextProps.results,
+      });
   }
-
-  setOtherUsers() {
-
-    let otherUsers = this.props.users.filter((index) => {
-
-      return ((index.uid !== this.props.uid) && (index.role !== this.state.currentUser.role));
-
-    });
-
-    this.setState({ users: otherUsers });
-
-  }
-
-  componentDidMount() {
-
-    this.componentDidUpdate();
-
-  }
-
-
-  componentDidUpdate() {
-
-    if (this.state.currentUser === undefined) {
-
-      this.setUser();
-
-    }
-
-    if (this.state.users.length === 0 && this.state.currentUser !== undefined && this.props.users.length > 0) {
-
-      this.setOtherUsers();
-      console.log(this.state.currentUser);
-    } else {
-
-      this.calcDistance();
-    }
-
-  }
-
-  calcDistance() {
-
-   /* var results = [];
-    var distance=[];
-    var R = 6371;      // km 
-    var lat=this.state.currentUser[0].lat;
-    var long=this.state.currentUser[0].lng;
-    var doubleLat=parseFloat(lat);
-    var doubleLong=parseFloat(long);
+  console.log("props", nextProps);
+  console.log("fghjkilo", JSON.stringify(this.state.results));
+}
   
-    function deg2rad(deg) {
-      return deg * (Math.PI/180)
-      }
-  
-    for(var i=0;i<this.state.users.length;i++){
-    var x1 = parseFloat(this.state.users[i].lat)-doubleLat;
-    var dLat = deg2rad(x1)  
-    var x2 = parseFloat(this.state.users[i].long)-doubleLong;
-    var dLon = deg2rad(x2);  
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-                    Math.cos(deg2rad(doubleLat)) * Math.cos(deg2rad(this.state.users[i].lat)) * 
-                    Math.sin(dLon/2) * Math.sin(dLon/2);  
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c; 
-    console.log("a: ",a);
-    console.log("C:",c);
-    console.log("D", d);
-    if(d<200){
-      results.push(this.state.users[i]);
-      distance.push(this.state.users[i]);
-      
-    }
-    }
-    console.log("Results ",results);
-*/
-  }
 
   render() {
-   
-    /*var results=[];   
-    //function
-        var R = 6371;      // km 
-    
-      var lat=this.state.currentUser[0].lat;
-      var long=this.state.currentUser[0].lng;
-      var doubleLat=parseFloat(lat);
-      var doubleLong=parseFloat(long);
-    
-      function deg2rad(deg) {
-        return deg * (Math.PI/180)
-        }
-    
-      for(var i=0;i<this.state.users.length;i++){
-      var x1 = parseFloat(this.state.users[i].lat)-doubleLat;
-      var dLat = deg2rad(x1)  
-      var x2 = parseFloat(this.state.users[i].long)-doubleLong;
-      var dLon = deg2rad(x2);  
-      var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-                      Math.cos(deg2rad(doubleLat)) * Math.cos(deg2rad(this.state.users[i].lat)) * 
-                      Math.sin(dLon/2) * Math.sin(dLon/2);  
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-      var d = R * c; 
-      console.log("a: ",a);
-      console.log("C:",c);
-      console.log("D", d);
-      console.log(results.push(d));
-      }
-      console.log("Results ",results);
-    */
+   console.log(3);
     return (
       <div>
         <Grid container columns={3}>
-          {this.state.users.map((user, index) => (
+          {this.state.results.map((user, index) => (
             <Grid.Column key={index}>
               <Card>
                 <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
                 <Card.Content>
                   <Card.Header>{user.firstName} {user.lastName}</Card.Header>
+                  <Card.Description>Distance from you: {user.distance} m</Card.Description>
+                  <Button inverted color='green'>Request a ride</Button>
                 </Card.Content>
               </Card>
             </Grid.Column>
