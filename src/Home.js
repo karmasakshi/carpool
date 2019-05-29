312fe3bd56207bd1274e44568a70c5ae6e51bd84
Anthony Trad <Anthony.trad@sapientrazorfish.com>
2019-05-29 15:14:52 +0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import "./index.css"
 import { Link } from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
  
 class Home extends Component {
  
   render() {
+    var signIntoDashBoard=<Link className="ui green button" id="style" to="/dashboard">Join Now</Link>
+    var signUp=<Link className="ui green button" id="style" to="/sign-up">Join Now</Link>
+
+    
     return (
       <div>
-        <Link className="ui green button" id="style" to="/sign-up">Join Now</Link>
+        {this.state.authUser ? signIntoDashBoard : signUp}
         <br />
         <p id="tagline">Join Hands for a Greener Tomorrow!</p>
       </div>
     );
   }
+
 }
  
 export default Home;
