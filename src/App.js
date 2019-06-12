f28548c7a7101a12cc0555ade1e79499fd18b5a4
Anthony Trad <Anthony.trad@sapientrazorfish.com>
2019-06-12 17:25:31 +0400

    Fix setState for SignUp component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import '../node_modules/semantic-ui-css/semantic.min.css';
 import fire from './config/fire';
 import GuestDashboard from './GuestDashboard';
 import HostDashboard from './HostDashboard';
-import { MyProvider } from './Context.js';
 
 class App extends Component {
 
@@ -19,19 +18,9 @@ class App extends Component {
     appUser: null
   };
 
-  update(eve) {
-    this.setState({ eve: this.state.appUser });
-  }
-  componentWillUnmount() {
-    fire.database().ref('users').off();
-
-  }
-
   componentDidMount() {
 
     fire.auth().onAuthStateChanged((authUser) => {
-
-      console.log('authUser', authUser);
 
       if (authUser) {
 
@@ -42,48 +31,38 @@ class App extends Component {
           if (appUser) {
 
             this.setState({ authUser: authUser, appUser: appUser });
-            console.log("2");
 
           } else {
-            this.setState({ authUser: null, appuser: null })
+            this.setState({ authUser: authUser, appuser: null })
           }
-
-
-
         });
-
       }
-
       else {
-
         this.setState({ authUser: null, appUser: null });
-
       }
-
     });
+  }
 
+  componentWillUnmount() {
+    fire.database().ref('users').off();
   }
 
   render() {
-    console.log(",", this.state.appUser);
     return (
-      <MyProvider appUser={this.state.appUser} authUser={this.state.authUser}>
-        <BrowserRouter>
-          <div>
-            <Navbar authUser={this.state.authUser} appUser={this.state.appUser} />
-            <Switch>
-              <Route exact path='/' component={() => <Home appUser={this.state.appUser} authUser={this.state.authUser} />} />
-              <Route exact path='/sign-in' component={() => <SignIn authUser={this.state.authUser} appUser={this.state.appUser} />} />
-              <Route exact path='/guest-dashboard' render={() => <GuestDashboard appUser={this.state.appUser} authUser={this.state.authUser} />} />
-              <Route exact path='/host-dashboard' render={() => <HostDashboard appUser={this.state.appUser} authUser={this.state.authUser} />} />
-              <Route exact path='/sign-up' component={() => <CreateAccount authUser={this.state.authUser} />} />
-              <Route exact path='/create-profile' component={() => <CreateProfile authUser={this.state.authUser} appUser={this.state.appUser} />} />
-              <Route render={() => <h1>Page not found</h1>} />
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </MyProvider>
-
+      <BrowserRouter>
+        <div>
+          <Navbar authUser={this.state.authUser} appUser={this.state.appUser} />
+          <Switch>
+            <Route exact path='/' component={() => <Home appUser={this.state.appUser} authUser={this.state.authUser} />} />
+            <Route exact path='/sign-in' component={() => <SignIn authUser={this.state.authUser} appUser={this.state.appUser} />} />
+            <Route exact path='/guest-dashboard' render={() => <GuestDashboard appUser={this.state.appUser} authUser={this.state.authUser} />} />
+            <Route exact path='/host-dashboard' render={() => <HostDashboard appUser={this.state.appUser} authUser={this.state.authUser} />} />
+            <Route exact path='/sign-up' component={() => <CreateAccount authUser={this.state.authUser} />} />
+            <Route exact path='/create-profile' component={() => <CreateProfile authUser={this.state.authUser} appUser={this.state.appUser} />} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
