f12f6c09850155bc3ca71effaaeddf1eae33f840
Shivani <shivani.chelliah@publicissapient.com>
2019-06-18 12:18:38 +0400

    remove cloud functions

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,19 @@
 import React, { Component } from "react";
 import "./index.css"
 import { Link } from 'react-router-dom'
-import {Redirect} from 'react-router-dom'
  
 class Home extends Component {
  
   render() {
-    var signIntoDashBoard=<Link className="ui green button" id="style" to="/dashboard">Join Now</Link>
     var signUp=<Link className="ui green button" id="style" to="/sign-up">Join Now</Link>
 
-    
-    return (
-      <div>
-        {this.props.authUser ? signIntoDashBoard : signUp}
+      <div id='home'>
+        {this.props.appUser!==null ? ((this.props.appUser.role === 'guest')?<Link className="ui green button" id="style" to="/guest-dashboard">Dashboard</Link> :<Link className="ui green button" id="style" to="/host-dashboard">Dashboard</Link>): signUp}
         <br />
         <p id="tagline">Join Hands for a Greener Tomorrow!</p>
       </div>
     );
   }
-
 }
  
 export default Home;
--- src/index.js
+++ src/index.js
@@ -3,28 +3,11 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import "./index.css";
 import App from './App';
-import functions from 'firebase-functions';
-import exports from 'exports';
-import fire from './config/fire';
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-import admin from 'firebase-admin';
-admin.initializeApp();
-
-// Realtime Database under the path /messages/:pushId/original
-
 
 ReactDOM.render(
   <App />,
   document.getElementById("root")
 );
-
-//var dt = new Date();
-//dt.setDate( dt.getDate() - 1 );
-
-//var db = fire.database();
-//exports.deleteRequests = functions.database.ref('/Requests/').orderByChild('dateOfJourney').endAt(dt).onDelete();
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
