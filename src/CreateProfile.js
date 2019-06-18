b4b3dfc096a6c257e490e7eb0625158d754f5c97
Shivani <shivani.chelliah@publicissapient.com>
2019-06-18 12:16:09 +0400

    activate redirect upon submission of form

--- src/CreateProfile.js
+++ src/CreateProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Segment, Grid, Image, Form, Radio, Loader, Dimmer } from 'semantic-ui-react';
+import { Segment, Grid, Image, Form, Icon, Header, Radio, Container } from 'semantic-ui-react';
 import fire from './config/fire';
-import { Redirect } from 'react-router';
+import { Redirect } from 'react-router-dom'
 
 class CreateProfile extends Component {
 
@@ -13,28 +13,26 @@ class CreateProfile extends Component {
       lat: null,
       lng: null,
     },
-    isLoading: false,
-    isGeolocationLoading: false,
-    isLocationAvailable: false,
-    updateProfile: null
+    createdProfile: false
   }
 
   createProfile = (event) => {
 
     event.preventDefault();
 
-    if (!this.state.isLoading) {
-      this.setState({ isLoading: true });
-      console.log(this.props.authUser.uid);
-      fire.database().ref('/users/' + this.props.authUser.uid).set({
-        firstName: this.state.user.firstName,
-        lastName: this.state.user.lastName,
-        role: this.state.user.role,
-        lat: this.state.user.lat,
-        lng: this.state.user.lng,
-        id: this.props.authUser.uid
-      })
-    }
+    this.setState({ createdProfile: true });
+
+    fire.database().ref('/users/' + this.props.authUser.uid).set({
+      firstName: this.state.user.firstName,
+      lastName: this.state.user.lastName,
+      role: this.state.user.role,
+      lat: this.state.user.lat,
+      lng: this.state.user.lng,
+      id: this.props.authUser.uid
+    })
+
+    this.props.getAppUserAfterRegistration(this.state.user);
+
   }
 
   updateInputs = (event) => {
@@ -55,8 +53,6 @@ class CreateProfile extends Component {
 
         var lat = position.coords.latitude;
         var lng = position.coords.longitude;
-        console.log(lat);
-        console.log(lng);
 
         var state = this.state;
         state.user.lat = lat;
@@ -74,67 +70,86 @@ class CreateProfile extends Component {
   }
 
   render() {
+    if (this.state.createdProfile === true && this.state.user.role === 'guest') {
+      return <Redirect to='/guest-dashboard' />
+    }
+    else if (this.state.createdProfile === true && this.state.user.role === 'host') {
+      return <Redirect to='/host-dashboard' />
+    }
+    else {
+      return (
+        <div>
+          <Container>
+            <Grid>
+              <Grid.Column width={6}>
+                <h1> Create Profile </h1>
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <br />
+                <br />
+                <Segment>
+                  <Form>
+                    <Form.Group widths='equal'>
+                      {this.state.firstNameValid}
+                      <Form.Input fluid name='firstName' type="text" onChange={this.updateInputs} value={this.state.user.firstName} label='First name' placeholder='First name' required />
+                      {this.state.lastNameValid}
+                      <Form.Input fluid name='lastName' type="text" onChange={this.updateInputs} value={this.state.user.lastName} label='Last name' placeholder='Last name' required />
+                    </Form.Group>
 
-    return (
-      <div>
-        <Grid>
-          <Grid.Column width={6}>
-            <h1> Create Profile </h1>
-            <Image src='' size='medium' centered bordered />
-          </Grid.Column>
-          <Grid.Column width={8}>
-            <br />
-            <br />
-            <Segment>
-              <Form>
-                <Form.Group widths='equal'>
-                  {this.state.firstNameValid}
-                  <Form.Input fluid name='firstName' type="text" onChange={this.updateInputs} value={this.state.user.firstName} label='First name' placeholder='First name' required />
-                  {this.state.lastNameValid}
-                  <Form.Input fluid name='lastName' type="text" onChange={this.updateInputs} value={this.state.user.lastName} label='Last name' placeholder='Last name' required />
-                </Form.Group>
+                    <Form.Group inline>
+                      <label>User Role</label>
+                      <Form.Radio
+                        label='Host'
+                        value='host'
+                        name='role'
+                        type="radio"
+                        id="host"
+                        checked={this.state.user.role === 'host'}
+                        onChange={(e) => {// copy state, modify copy, set state to modified copy
+                          var state = this.state; state.user.role = e.target.value; this.setState(state)
+                        }}
+                      ></Form.Radio>
+                      <Form.Radio
+                        label='Guest'
+                        value='guest'
+                        name='role'
+                        type="radio"
+                        id="guest"
+                        checked={this.state.user.role === 'guest'}
+                        onChange={(e) => { var state = this.state; state.user.role = e.target.value; this.setState(state) }}
+                      ></Form.Radio>
+                    </Form.Group>
 
-                <Form.Group inline>
-                  <label>User Role</label>
-                  <Form.Radio
-                    label='Host'
-                    value='host'
-                    name='role'
-                    type="radio"
-                    id="host"
-                    checked={this.state.user.role === 'host'}
-                    onChange={(e) => {// copy state, modify copy, set state to modified copy
-                      var state = this.state; state.user.role = e.target.value; this.setState(state)
-                    }}
-                  ></Form.Radio>
-                  <Form.Radio
-                    label='Guest'
-                    value='guest'
-                    name='role'
-                    type="radio"
-                    id="guest"
-                    checked={this.state.user.role === 'guest'}
-                    onChange={(e) => { var state = this.state; state.user.role = e.target.value; this.setState(state) }}
-                  ></Form.Radio>
-                </Form.Group>
+                    <Form.Button onClick={this.getLocation}>Get My Location</Form.Button>
 
-                <Form.Button onClick={this.getLocation}>Get My Location</Form.Button>
-
-                <Form.Button type="submit" disabled={this.state.user.firstName === '' || this.state.user.lastName === '' || this.state.user.lng === null || this.state.user.lat === null || this.state.user.role === null} onClick={this.createProfile} >Submit</Form.Button>
-              </Form>
-            </Segment>
-
-          </Grid.Column>
-        </Grid>
-      </div>
-    );
-
+                    <Form.Button type="submit" disabled={this.state.user.firstName === '' || this.state.user.lastName === '' || this.state.user.lng === null || this.state.user.lat === null || this.state.user.role === null} onClick={this.createProfile} >Submit</Form.Button>
+                  </Form>
+                </Segment>
+              </Grid.Column>
+            </Grid>
+          </Container>
+        </div>
+      );
+    }
   }
-
 }
-
-
 
 export default CreateProfile;
 
 
+/*
+import Dropzone from 'react-dropzone'
+<br />
+              <Icon name='upload' size='huge'/>
+              <Header content="Drop Image Here"/>
+              <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                {({ getRootProps, getInputProps }) => (
+                  <section className="container">
+                    <div {...getRootProps()}>
+                      <input {...getInputProps()} />
+                      <p>Drag 'n' drop some files here, or click to select files</p>
+                    </div>
+                  </section>
+                )}
+              </Dropzone>
+              */
